26a26b85336793487eb06ee159f852df0ca68dab
Edouard Marquez <moi@edouard-marquez.me>
2018-03-23 07:39:19 +0100

    Support for the Apple App Store Note: It would be great to implement the SKStoreProductViewController

--- ios/Classes/LaunchReviewPlugin.m
+++ ios/Classes/LaunchReviewPlugin.m
@@ -2,19 +2,35 @@
 
 @implementation LaunchReviewPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"launch_review"
-            binaryMessenger:[registrar messenger]];
-  LaunchReviewPlugin* instance = [[LaunchReviewPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"launch_review"
+                                     binaryMessenger:[registrar messenger]];
+    LaunchReviewPlugin* instance = [[LaunchReviewPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"launch" isEqualToString:call.method]) {
+        NSString *appId = call.arguments[@"ios_id"];
+        if (!appId.length) {
+            result([FlutterError errorWithCode:@"ERROR"
+                                       message:@"Invalid app id"
+                                       details:nil]);
+        } else {
+            NSString* iTunesLink;
+            if([[[UIDevice currentDevice] systemVersion] floatValue] >= 11) {
+                iTunesLink = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/xy/app/foo/id%@?action=write-review", appId];
+            } else {
+                iTunesLink = [NSString stringWithFormat:@"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@&action=write-review", appId];
+            }
+            
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:iTunesLink]];
+            
+            result(nil);
+        }
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 @end
