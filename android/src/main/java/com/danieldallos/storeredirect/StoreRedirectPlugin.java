9c2d0731f5581f2c8b1a74667d46a2f4883b0746
Ewerton Ramos <ewerton.ramos@crmall.com>
2020-10-09 15:52:29 -0300

    Add support to new Android plugins APIs

--- android/src/main/java/com/danieldallos/storeredirect/StoreRedirectPlugin.java
+++ android/src/main/java/com/danieldallos/storeredirect/StoreRedirectPlugin.java
@@ -1,31 +1,50 @@
 package com.danieldallos.storeredirect;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
 /**
  * StoreRedirectPlugin
  */
-public class StoreRedirectPlugin implements MethodCallHandler {
+public class StoreRedirectPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
 
-  private final Registrar mRegistrar;
-
-  private StoreRedirectPlugin(Registrar registrar) {
-    this.mRegistrar = registrar;
-  }
+  private Activity activity;
+  private MethodChannel methodChannel;
 
   /**
    * Plugin registration.
    */
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "store_redirect");
-    StoreRedirectPlugin instance = new StoreRedirectPlugin(registrar);
-    channel.setMethodCallHandler(instance);
+    StoreRedirectPlugin instance = new StoreRedirectPlugin();
+    instance.onAttachedToEngine(registrar.messenger());
+  }
+
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding binding) {
+    onAttachedToEngine(binding.getBinaryMessenger());
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+    methodChannel.setMethodCallHandler(null);
+    methodChannel = null;
+  }
+
+  private void onAttachedToEngine(BinaryMessenger messenger) {
+    methodChannel = new MethodChannel(messenger, "store_redirect");
+    methodChannel.setMethodCallHandler(this);
   }
 
   @Override
@@ -38,17 +57,37 @@ public class StoreRedirectPlugin implements MethodCallHandler {
       if (appId != null) {
         appPackageName = appId;
       } else {
-        appPackageName = mRegistrar.activity().getPackageName();
+        appPackageName = this.activity.getPackageName();
       }
 
       Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName));
       marketIntent.addFlags(
           Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-      mRegistrar.activity().startActivity(marketIntent);
+      this.activity.startActivity(marketIntent);
 
       result.success(null);
     } else {
       result.notImplemented();
     }
+  }
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    this.activity = binding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+    onDetachedFromActivity();
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+    onAttachedToActivity(binding);
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    this.activity = null;
   }
 }
--- example/android/app/src/main/java/com/iyaffle/launchreviewexample/MainActivity.java
+++ example/android/app/src/main/java/com/iyaffle/launchreviewexample/MainActivity.java
@@ -6,9 +6,5 @@ import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+
 }
