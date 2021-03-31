8826119dd82cb5114bc6c51a6095ab57d3bda24d
lukaskurz <lukaskurz5@gmail.com>
2021-03-31 02:15:06 +0200

    add null-safety

--- lib/store_redirect.dart
+++ lib/store_redirect.dart
@@ -6,7 +6,7 @@ class StoreRedirect {
   static const MethodChannel _channel = const MethodChannel('store_redirect');
 
   /// Note: It will not work with the iOS Simulator
-  static Future<void> redirect({String androidAppId, String iOSAppId}) async {
+  static Future<void> redirect({String? androidAppId, String? iOSAppId}) async {
     await _channel.invokeMethod(
         'redirect', {'android_id': androidAppId, 'ios_id': iOSAppId});
     return null;
