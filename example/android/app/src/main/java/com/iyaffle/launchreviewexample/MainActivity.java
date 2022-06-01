c9ccdfa3e0ba86f051e0314d0dca25a827692e1e
amin tajdarian <58747585+amintajdarian@users.noreply.github.com>
2022-06-01 12:58:42 +0430

    migrate to flutter 3

--- example/android/app/src/main/java/com/iyaffle/launchreviewexample/MainActivity.java
+++ example/android/app/src/main/java/com/iyaffle/launchreviewexample/MainActivity.java
@@ -2,7 +2,7 @@ package com.iyaffle.launchreviewexample;
 
 import android.os.Bundle;
 
-import io.flutter.app.FlutterActivity;
+import  io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
